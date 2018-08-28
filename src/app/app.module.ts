bc7a235c1717947d03a77cb220c2db01c89d0803
Mason Secky-Koebel <mseckykoebel@gmail.com>
2018-08-28 12:11:33 -0400

    added the navbar to the top, as well as routed the home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,36 @@ import { AngularFireDatabaseModule } from "angularfire2/database";
 import { AngularFireAuthModule } from "angularfire2/auth";
 
 import { AppComponent } from "./app.component";
+import { environment } from "../environments/environment.prod";
+import { BsNavbarComponent } from "./bs-navbar/bs-navbar.component";
+
+import { RouterModule } from "@angular/router";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { APP_BASE_HREF } from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot([
+      { path: "", component: HomeComponent },
+      { path: "login", component: LoginComponent }
+    ])
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: "/"}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
